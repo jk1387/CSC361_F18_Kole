6639d272c693737aa8d550fb6958dae3202418bc
Jacob Kole <jk1387@ship.edu>
2018-11-15 09:12:54 -0500

    Shard score is handled, bodies are destroyed, can be collected one by one. Need to link my clock to my time in relation to score.

--- core/src/com/packtpub/libgdx/light/game/objects/AbstractGameObject.java
+++ core/src/com/packtpub/libgdx/light/game/objects/AbstractGameObject.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.packtpub.libgdx.light.game.WorldController;
@@ -31,6 +32,9 @@ public abstract class AbstractGameObject {
 	public Body body;
 	public BodyDef bodyDef;
 	public PolygonShape poly;
+	public Fixture fixture;
+	
+	public boolean collected;
 	
 	/**
 	 * Constructer to initialize the vectors for the object
